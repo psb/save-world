784733e694076959b4b60b4005abb09a1f98f499
Adnan Wahab <adnan@wahab.(none)>
2013-01-15 15:21:36 -0800

    game loops working

--- save-world.js
+++ save-world.js
@@ -1,27 +1,36 @@
+var Game = new Meteor.Collection('game');
 var Maps = new Meteor.Collection('maps');
 var Players = new Meteor.Collection('players');
 
 if (Meteor.isClient) {
-  Session.set('answer', 'NY');
+  Meteor.autosubscribe(function() {
+    Game.find().observe({
+      changed: function(item){ 
+        console.log(10)
+      }
+    })
+  })
 
-  var restart = function(){
-    Players.update( {}, {$set: {guesses: 10}}, {multi: true} );
-    Session.get('currentPlayer').guesses = 10;
-  };
+  // Meteor.call('win'); 
 
+  Meteor.startup(function () {  
+    window.next()
+    setInterval(window.next, 5000);
+  })
   var checkAnswer = function(player, guess){
     console.log(player, guess);
     if ( Session.equals('answer', guess) ){
       Players.update( {_id: player._id}, {$inc: {score: +10}} );
-      restart();
+      window.next();
     }
   };
 
-  Template.currentPlayer.username = function(){
-    var user =  Session.get('currentPlayer');
-    return Players.findOne(user);
-  };
-  
+  Template.currentPlayer.events({
+    'blur .username': function (e) {
+      Players.update( {_id:this._id}, {name: e.srcElement.innerText} );
+    }
+  });
+
   Template.currentPlayer.score = function(){
     var user =  Session.get('currentPlayer');
     return Players.findOne(user);
@@ -53,13 +62,11 @@ if (Meteor.isClient) {
 
   Template.guess.events = {
     'keypress #guess': function(evt, template){
-      if (evt.which === 13) {
-        var answer = template.find('#guess').value;
-        console.log(template);
-        Players.update( this, {$inc: {guesses: -1}} );
-        checkAnswer(this, answer);
-        template.find('#guess').value = '';
-      }
+      if (evt.which !== 13) return;
+      var answer = evt.srcElement.value;
+      Players.update( this, {$inc: {guesses: -1}} );
+      checkAnswer(this, answer);
+      template.find('#guess').value = '';
     }
   };
 
@@ -69,11 +76,32 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  var start_round = function () {
+    var number_of_countries = 177;
+    var i = ~~(Math.random()  * number_of_countries);
+    Game.update({}, {num: i})
+    console.log(i)
+  }
+
+  var uck = Meteor.setInterval(start_round, 5000);
+  console.log(typeof uck)
+
+  Meteor.methods({
+    win: function () {
+      console.log('you win !!!' +  id)
+      Meteor.clearInterval(id);
+      start_round();
+      id = Meteor.setInterval(start_round, 5000);
+    }
+  });
   Meteor.startup(function () {
+
+    if (Game.find().count() === 0) Game.insert ({num: 0});
+
     if (Players.find().count() === 0){
-      Players.insert({ username: 'Lord Pippen', score: 0, location: 'United-Kingdom', guesses: 10 });
-      Players.insert({ username: 'Davis the 3rd', score: 0, location: 'United-States', guesses: 10 });
-      Players.insert({ username: 'Alf', score: 0, location: 'Cocos-Keeling-Islands', guesses: 10 });
+      Players.insert({ username: 'Lord Pippen', score: 0, location: 'United-Kingdom'});
+      Players.insert({ username: 'Davis the 3rd', score: 0, location: 'United-States'});
+      Players.insert({ username: 'Alf', score: 0, location: 'Cocos-Keeling-Islands'});
     }
   });
 }
